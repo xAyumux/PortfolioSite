66d6d6d8523f3ec1d9e4e9bb25faba5cce91a2d4
xAyumux <ayumu.f.1127@gmail.com>
2022-06-02 19:49:19 +0900

    Change about.tsx

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -1,11 +1,15 @@
+import Link from 'next/link';
+
 function About() {
   return (
     <div>
       <h1>About</h1>
+      <Link href='/'>
+        <p>index</p>
+      </Link>
       <p>test</p>
       <p>test</p>
       <p>test</p>
-      <p>index</p>
     </div>
   );
 }
